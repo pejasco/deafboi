33603dff3fdde1772fc6af41109f408d727b976e
siev <pejanicbestfrd@gmail.com>
2024-11-04 01:18:29 +0000

    setting up cart and common 4 Nov 1:18am

--- src/sdl3.cpp
+++ src/sdl3.cpp
@@ -3,33 +3,34 @@
 //manage the main entry point in SDL3. which my be required on some platforms
 // we used to need to use the sdl2 main linking library
 #include <iostream>
+#include "../include/Common.hpp"
 
 int main()
 {
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
 
-    int result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-    if (result < 0)
+    bool result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+    if (result == FALSE)
     {
-        std::cerr << "Unable to initialize SDL: " << SDL_GetError() << std::endl;
+        std::cerr << "Unable to initialize SDL: " << SDL_GetError() << "\n";
         return -1;
     }
-    window = SDL_CreateWindow("muteboi", 500, 500, 0);
+    window = SDL_CreateWindow(WINDOW_NAME, WINDOW_W, WINDOW_H, 0);
     if (window == nullptr)
     {
-        std::cerr << "Unable to create window: " << SDL_GetError() << std::endl;
+        std::cerr << "Unable to create window: " << SDL_GetError() << "\n";
         return -2;
     }
 
     renderer = SDL_CreateRenderer(window, NULL);
     if (renderer == nullptr)
     {
-        std::cerr << "Unable to create renderer: " << SDL_GetError() << std::endl;
+        std::cerr << "Unable to create renderer: " << SDL_GetError() << "\n";
         return -3;
     }
 
-    std::cout << "msg: SDL3 initialized successfully" << std::endl;
+    std::cout << "msg: SDL3 initialized successfully" << "\n";
 
     SDL_Event event;
     bool quit = false;
@@ -40,13 +41,13 @@ int main()
             switch (event.type)
             {
                 case SDL_EVENT_QUIT:
-                    std::cout << "msg: SDL3 event quit" << std::endl;
+                    std::cout << "msg: SDL3 event quit" << "\n";
                     quit = true;
                     break;
                 case SDL_EVENT_KEY_DOWN:
                     if (event.key.key == SDLK_ESCAPE)
                     {
-                        std::cout << "msg: SDL3 event quit" << std::endl;
+                        std::cout << "msg: SDL3 event quit" << "\n";
                         quit = true;
                     }
                     break;  
@@ -59,7 +60,7 @@ int main()
         SDL_Delay(1); //add a slight delay to reduce CPU usage
     }
 
-    std::cout << "msg: SDL3 shutdown" << std::endl;
+    std::cout << "msg: SDL3 shutdown" << "\n";
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit(); // clean up SDL
